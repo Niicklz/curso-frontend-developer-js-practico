e9d886f548c7f16c548d151b03a03f5e1400c921
Niicklz <77895883+Niicklz@users.noreply.github.com>
2022-08-29 23:17:29 -0400

    ultimos cambios

--- main.js
+++ main.js
@@ -6,39 +6,34 @@ const carritoCompra = document.querySelector(".navbar-shopping-cart");
 const detallesCompra = document.querySelector(".product-detail");
 const flechita = document.querySelector(".flecha");
 
-
 emailMenu.addEventListener("click", toggleDesktopMenu);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle("inactive");
-    detallesCompra.classList.add("inactive");
-
-
+  desktopMenu.classList.toggle("inactive");
+  detallesCompra.classList.add("inactive");
 }
-
 
 menuIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleMobileMenu() {
-    menuMobile.classList.toggle("inactive");
-    detallesCompra.classList.add("inactive"); 
-    
-    console.log("click");
+  menuMobile.classList.toggle("inactive");
+  detallesCompra.classList.add("inactive");
+
+  console.log("click");
 }
 
 carritoCompra.addEventListener("click", toggleCompras);
 
 function toggleCompras() {
-    detallesCompra.classList.toggle("inactive");
-    menuMobile.classList.add("inactive"); 
-    desktopMenu.classList.add("inactive");  
+  detallesCompra.classList.toggle("inactive");
+  menuMobile.classList.add("inactive");
+  desktopMenu.classList.add("inactive");
 }
 
 flechita.addEventListener("click", toggleR);
 
 function toggleR() {
-   
-    detallesCompra.classList.toggle("inactive");
-    
-    console.log("clickazo");
+  detallesCompra.classList.toggle("inactive");
+
+  console.log("clickazo");
 }
